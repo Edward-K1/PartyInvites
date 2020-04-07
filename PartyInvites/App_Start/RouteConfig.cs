13ac01cb509357271cf86032512315c95f2b6346
Edward-K1 <eddyk1collab@gmail.com>
2020-04-07 20:04:37 +0300

    add home controller

--- PartyInvites/App_Start/RouteConfig.cs
+++ PartyInvites/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace PartyInvites
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
